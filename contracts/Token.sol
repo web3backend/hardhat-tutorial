9f03e2509bbeeca5a3a6cbda60831b5313316b6a
aminem2019 <aminemw29@gmail.com>
2023-11-13 09:30:42 +0000

    Adds debug info

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -49,6 +49,13 @@ contract Token {
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
 
+	console.log(
+		"Transferring from %s to %s %s tokens",
+		msg.sender,
+		to,
+		amount
+	);
+
         // Transfer the amount.
         balances[msg.sender] -= amount;
         balances[to] += amount;
